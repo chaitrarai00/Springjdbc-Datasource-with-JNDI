4728f722f033177ded4f4499f342a1ec14dcf6bf
chaitrarai00 <chaitrarai00@gmail.com>
2021-03-13 00:50:55 +0530

    JNDI support with datasource

---
+++ src/main/java/com/spring/jdbc/model/Employee.java
@@ -0,0 +1,30 @@
+package com.spring.jdbc.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable{
+	
+	private static final long serialVersionUID=-8882345425888374756L;
+	
+	private int id;
+	private String name;
+	private String role;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
